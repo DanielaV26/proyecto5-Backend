7ea08c0b4c4808e7a9e07f351e4648ff1f58b921
Carolina Hernandez <hernandez.carolina@live.cl>
2023-09-14 11:32:22 -0300

    [fix]: cors

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 import userRoutes from './src/routes/user.routes.js'
 import productRoutes from './src/routes/products.routes.js'
+import cors from 'cors'
 
 mongoose.connect(process.env.MONGO_URI+"brightbooks")
 
 const app = express()
 const PORT =  process.env.PORT  || 3000
-
+app.use(cors())
 app.use('/products', productRoutes)
 app.use('/user', userRoutes)
 
