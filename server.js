f2e633cef55e23365a295fc13ba4f47a6c0ff818
DanielaV26 <danielavargasgonzalez.v@gmail.com>
2023-10-04 15:06:59 -0300

    [fix]:server

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ import dotenv from 'dotenv'
 dotenv.config()
 import userRoutes from './src/routes/user.routes.js'
 import productRoutes from './src/routes/products.routes.js'
-import cors from 'cors'
 
 mongoose.connect(process.env.MONGO_URI+"brightbooks")
 
 const app = express()
 const PORT =  process.env.PORT  || 3000
-app.use(cors())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+  })
 app.use('/products', productRoutes)
 app.use('/user', userRoutes)
 
