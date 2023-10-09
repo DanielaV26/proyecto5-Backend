fbd29006d86b231d0688e206ec7ce5ec4823171e
Carolina Hernandez <hernandez.carolina@live.cl>
2023-10-09 17:53:16 -0300

    [fix]: Token

--- src/helpers/generateToken.js
+++ src/helpers/generateToken.js
@@ -3,9 +3,9 @@ import jsonwebtoken from 'jsonwebtoken'
 const secret = process.env.JWT_SECRET_KEY;
 
 export const generateToken = (user) => {
-  const { _id, userName, email, isAdmin } = user;
+  const { _id, name, lastname, email, identification, age, country, region, commune, address, phone } = user;
   return jsonwebtoken.sign(
-    { _id, userName, email, isAdmin },
+    { _id, name, lastname, email, identification, age, country, region, commune, address, phone},
     secret,
     { expiresIn: '30d' }
   );
